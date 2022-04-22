1d0e67662e362097c160eeb9b1970e77029d932e
Rama <rama.alshaban@gmail.com>
2022-04-22 05:29:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tutorial) => {
+   const tutorialToArr = tutorial.split(' '); 
+        return tutorialToArr.map((word) => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(' ');
+ })
 }
+
